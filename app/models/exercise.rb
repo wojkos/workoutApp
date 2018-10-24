ea5382172a6360d8ed044015e13bca9a0cbecdd2
wojtek.kostanski <wojtek.kostanski@gmail.com>
2018-10-24 22:08:59 +0200

    add exercise model validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,7 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+
+  validates :duration, numericality: { greater_than: 0.0 }
+  validates :workout_details, presence: true
+  validates :activity_date, presence: true
 end
