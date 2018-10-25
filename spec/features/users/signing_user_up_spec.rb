65059d3c1d236a1da6a4d8d3f98a0f2819769fce
wojtek.kostanski <wojtek.kostanski@gmail.com>
2018-10-25 22:30:08 +0200

    add first and last name for user

--- spec/features/users/signing_user_up_spec.rb
+++ spec/features/users/signing_user_up_spec.rb
@@ -6,10 +6,15 @@ RSpec.feature "Users signup" do
    
     visit "/"
     click_link "Sign up"
+    fill_in "First name", with: "Jon"
+    fill_in "Last name", with: "Example"
     fill_in "Email", with: "john@example.com"
     fill_in "Password",  with: "password"
     fill_in "Password confirmation",  with: "password"
     click_button "Sign up"
     expect(page).to have_content("You have signed up successfully.")
+
+    visit '/'
+    expect(page).to have_content('Jon Example')
   end
 end
