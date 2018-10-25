48114c5cce28a89352985c8738fa1c95aa024a64
wojtek.kostanski <wojtek.kostanski@gmail.com>
2018-10-25 22:42:19 +0200

    validate first and last name

--- spec/features/users/signing_user_up_spec.rb
+++ spec/features/users/signing_user_up_spec.rb
@@ -17,4 +17,19 @@ RSpec.feature "Users signup" do
     visit '/'
     expect(page).to have_content('Jon Example')
   end
+
+  scenario "with invalid credentials" do
+    visit "/"
+
+    click_link "Sign up"
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password",  with: "password"
+    fill_in "Password confirmation",  with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+  end
 end
