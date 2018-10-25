cfce0940a6af43b844c9cfd865471fba1e909c18
wojtek.kostanski <wojtek.kostanski@gmail.com>
2018-10-26 00:05:04 +0200

    test incasesensitive search

--- spec/features/dashboards/users_searching_spec.rb
+++ spec/features/dashboards/users_searching_spec.rb
@@ -11,9 +11,13 @@ RSpec.feature "Searching for User" do
                           last_name: "Doe",
                           email: "sarah@example.com",
                           password: "password")
+    @leo = User.create!(first_name: "Leo", 
+                          last_name: "Example",
+                          email: "leo@example.com",
+                          password: "password")
   end
   
-  scenario "with existing name returns all those users" do
+  scenario "with existing name returns only those users" do
     visit '/'
     
     fill_in "search_name", with: "Doe"
@@ -21,7 +25,26 @@ RSpec.feature "Searching for User" do
     
     expect(page).to have_content(@john.full_name)
     expect(page).to have_content(@sarah.full_name)
+    expect(page).not_to have_content(@leo.full_name)
     expect(current_path).to eq("/dashboards/search")
+  end
+
+  scenario "with incasesensitive search input" do
+    visit '/'
+    
+    fill_in "search_name", with: "sarah"
+    click_button "Search"
+    
+    expect(page).to have_content(@sarah.full_name)
+    expect(page).not_to have_content(@john.full_name)
+    expect(page).not_to have_content(@leo.full_name)
+
+    fill_in "search_name", with: "LEO"
+    click_button "Search"
+    
+    expect(page).to have_content(@leo.full_name)
+    expect(page).not_to have_content(@john.full_name)
+    expect(page).not_to have_content(@sarah.full_name)
   end
  
 end
