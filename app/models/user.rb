fed79a187d7049c4973a6a0113c73f9de3d5aec1
wojtek.kostanski <wojtek.kostanski@gmail.com>
2018-10-25 23:58:43 +0200

    implement user search

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,18 @@ class User < ApplicationRecord
   def full_name
     [first_name, last_name].join(' ')
   end
+
+  def self.search_by_name(name)
+    names_array = name.split(' ')
+    names_array.map! { |x| x.downcase }
+    if names_array.size == 1
+      where('lower(first_name) LIKE ? or lower(last_name) LIKE ?',
+        "%#{names_array[0]}%", "%#{names_array[0]}%").order(:first_name)
+    else
+      where('lower(first_name) LIKE ? or lower(first_name) LIKE ? or lower(last_name) LIKE ?
+        or lower(last_name) LIKE ?', "%#{names_array[0]}%",
+        "%#{names_array[1]}%", "%#{names_array[0]}%",
+        "%#{names_array[1]}%").order(:first_name)
+    end
+  end
 end
