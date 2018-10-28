3ebbc8ded65e07b2bf2d2553d00ba6b989711a74
wojtek.kostanski <wojtek.kostanski@gmail.com>
2018-10-28 20:46:12 +0100

    add following friend feature

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,26 @@
+class FriendshipsController < ApplicationController
+  before_action :authenticate_user!
+
+def show
+end
+
+def create
+  friend = User.find(params[:friend_id])
+  params[:user_id] = current_user.id
+  Friendship.create!(friendship_params) unless current_user.follow_or_same?(friend)
+  redirect_to root_path
+end
+
+def destroy
+end
+
+private
+
+# def set_exercise
+#   @exercise = current_user.exercises.find(params[:id])
+# end
+
+def friendship_params
+  params.permit(:friend_id, :user_id)
+end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_194105) do
+ActiveRecord::Schema.define(version: 2018_10_28_193033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2018_10_24_194105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_exercises_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +49,5 @@ ActiveRecord::Schema.define(version: 2018_10_24_194105) do
   end
 
   add_foreign_key "exercises", "users"
+  add_foreign_key "friendships", "users"
 end
