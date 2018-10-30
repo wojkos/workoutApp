941435f1bdcfa11e3647920a7a8599e1a2595d6a
wojtek.kostanski <wojtek.kostanski@gmail.com>
2018-10-30 21:56:00 +0100

    working on action cable connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identyfied_by :current_user
+    def connect
+      set.current_user = find_current_user
+    end
+
+
+    def find_current_user
+      if current_user = User.find_by(id: cookies.signal['user_id'])
+        current_user
+      else
+        reject_unauthorizeed_connection
+      end
+    end
   end
 end
